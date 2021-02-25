789c8cff8d4d1da5577bae7749a914e52c2a6810
Wiktor Sadowski <wiktorsadowski.jd@gmail.com>
2021-02-25 08:18:47 +0100

    Create end of script.

--- script.sh
+++ script.sh
@@ -3,8 +3,17 @@ question_file = quiz_pytania.txt
 score = 0
 
 #Sprawdzamy czy są pytania
-if [ ! -f question_file ]
-then
-    echo "Nie mam pytań do quizu"
-    exit 1
+if [ ! -f question_file ]; then
+  echo "Nie mam pytan do quizu"
+  exit 1
+fi
+
+if [ $score ] >0; then
+  echo "Spróbuj ponownie kiedys :D"
+  echo "Twoja liczba punktow: $score/$(wc -l question_file)"
+fi
+
+else
+  echo "Gratulacje"
+  echo "Twoja liczba punktow: $score/$(wc -l question_file)"
 fi
