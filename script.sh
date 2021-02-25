a0cfe9122438af1f6e4e0d3f28cdb8675ae96533
Wiktor Sadowski <wiktorsadowski.jd@gmail.com>
2021-02-25 09:11:14 +0100

    Create loop which print question and answers.

--- script.sh
+++ script.sh
@@ -7,8 +7,38 @@ if [ ! -f question_file ]; then
   echo "Nie mam pytan do quizu"
   exit 1
 fi
+#Glowna petla
+while read -u9 line
+do
+    # parsing quiz file for current question
+    question=`echo $line | cut -f1 -d' '`
+    choisces=
+    (
+    `echo $line | cut -f2 -d' '`
+    `echo $line | cut -f3 -d' '`
+    `echo $line | cut -f4 -d' '`
+    `echo $line | cut -f5 -d' '`)
 
-if [ $score ] >0; then
+
+
+    # wyswielt pytania i odpowiedzi
+    sub_string='TRUE'
+    echo "$question"
+    for choisce in "${choisces[@]}"
+    do
+      if [ $choisce == *"$sub_string*"]; then
+          simplifiedChoice = ${choisce::-4}
+          echo "$simplifiedChoice"
+      fi
+      else
+           echo "$choisce"
+      fi
+    done
+
+done 9< question_file
+
+
+if [ $score  >0 ]; then
   echo "Spróbuj ponownie kiedys :D"
   echo "Twoja liczba punktow: $score/$(wc -l question_file)"
 fi
